92def89fdc76cf20d6bd5be0afe6847ae11d9963
Adrià <adria@fsfe.org>
2017-01-07 13:53:08 +0100

    Beautify
    https://atom.io/packages/atom-beautify

--- static/monit-dashboard.js
+++ static/monit-dashboard.js
@@ -2,8 +2,8 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function(){
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("show");
-  }
+    acc[i].onclick = function() {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
 }
