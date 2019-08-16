bce596438fffeb51d0928045315b9e89a4b96c10
saravanan30erd <saravanan30erd@gmail.com>
2019-08-16 18:36:40 +0400

    add pie chart for showing services status

--- static/monit-dashboard.js
+++ static/monit-dashboard.js
@@ -7,3 +7,30 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
     }
 }
+
+function draw(data) {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+
+  var colors = ['#40ff00', '#ff0000'];
+  var angles = [Math.PI * 1.5, Math.PI * data];
+  var offset = 0;
+  var beginAngle = 0;
+  var endAngle = 0;
+  var offsetX, offsetY, medianAngle;
+
+  for(var i = 0; i < angles.length; i = i + 1) {
+    beginAngle = endAngle;
+    endAngle = endAngle + angles[i];
+    medianAngle = (endAngle + beginAngle) / 2;
+    offsetX = Math.cos(medianAngle) * offset;
+    offsetY = Math.sin(medianAngle) * offset;
+    ctx.beginPath();
+    ctx.fillStyle = colors[i % colors.length];
+    ctx.moveTo(200 + offsetX, 200 + offsetY);
+    ctx.arc(200 + offsetX, 200 + offsetY, 120, beginAngle, endAngle);
+    ctx.lineTo(200 + offsetX, 200 + offsetY);
+    ctx.stroke();
+    ctx.fill();
+  }
+}
